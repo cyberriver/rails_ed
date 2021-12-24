c0162560cededa4b99154612781520512c66caf7
cyberriver <vblagirev@gmail.com>
2021-12-24 08:34:06 +0300

    configured moscow time

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module TestGuru
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
+    config.time_zone = 'Moscow'
 
     # Configuration for the application, engines, and railties goes here.
     #
@@ -18,5 +19,10 @@ module TestGuru
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+   Time.zone      # => #<ActiveSupport::TimeZone:0x514834...>
+   Time.zone.name # => "Eastern Time (US & Canada)"
+   Time.zone.now  # => Sun, 18 May 2008 14:30:44 EDT -04:00
+
   end
 end
