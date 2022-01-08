95287215f0fb9a9c824954e4aec8045b82b35bf3
cyberriver <vblagirev@gmail.com>
2022-01-09 00:34:23 +0300

    rollback once more

--- db/schema.rb
+++ db/schema.rb
@@ -10,54 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_31_170442) do
+ActiveRecord::Schema.define(version: 0) do
 
-  create_table "answers", force: :cascade do |t|
-    t.string "title", limit: 1000, null: false
-    t.boolean "correct", default: false
-    t.integer "user_id", null: false
-    t.integer "question_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["id"], name: "by answer", unique: true
-    t.index ["question_id"], name: "index_answers_on_question_id"
-    t.index ["user_id"], name: "index_answers_on_user_id"
-  end
-
-  create_table "categories", force: :cascade do |t|
-    t.string "title", limit: 1000, null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["id"], name: "by category", unique: true
-  end
-
-  create_table "questions", force: :cascade do |t|
-    t.string "body", limit: 1000, null: false
-    t.integer "tests_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["tests_id"], name: "index_questions_on_tests_id"
-  end
-
-  create_table "tests", force: :cascade do |t|
-    t.string "title", limit: 1000, null: false
-    t.integer "level", default: 0, null: false
-    t.integer "category_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["category_id"], name: "index_tests_on_category_id"
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "name", limit: 1000, null: false
-    t.string "email", limit: 500, null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["id"], name: "by user", unique: true
-  end
-
-  add_foreign_key "answers", "questions"
-  add_foreign_key "answers", "users"
-  add_foreign_key "questions", "tests", column: "tests_id"
-  add_foreign_key "tests", "categories"
 end
