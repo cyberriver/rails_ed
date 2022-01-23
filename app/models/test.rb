0a46f8718931f1605c299dae92d93c4b04d3172c
cyberriver <vblagirev@gmail.com>
2022-01-23 20:58:43 +0300

    excluded belongs to

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,5 +1,5 @@
 class Test < ApplicationRecord
-  belongs_to :category_id
+
   def self.by_category(category_name)
     joins('JOINS categories on category_id=category.id').where('categories.title=:category_name', category_name:category_name).order(title: :desc).pluck(:title)
 
