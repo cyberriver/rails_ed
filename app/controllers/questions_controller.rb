6476d2ebaab9b6688cbf18cc606ff873ff7d28a5
cyberriver <vblagirev@gmail.com>
2022-01-31 21:06:34 +0300

    finish homework for 7 lesson, Done rescue exception and callbacks

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,11 +1,17 @@
 class QuestionsController < ApplicationController
+  before_action :find_question, only: %i[show edit update destory]
+  before_action :find_test, only: %i[show]
+  after_action :send_log_message
+  around_action :log_execute_time
+
+  rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_question_not_found
+
   def index
     @question = Question.all
     #render json: {questions: Question.all}
   end
 
   def show
-    @question = Question.find(params[:id])
     render json: {question: @question}
 
   end
@@ -15,11 +21,10 @@ class QuestionsController < ApplicationController
   end
 
   def edit
-    @question = Question.find(params[:id])
+
   end
 
   def update
-    @question = Question.find(params[:id])
     @question.update!(body: params[:question][:body])
     redirect_to questions_path
   end
@@ -30,8 +35,7 @@ class QuestionsController < ApplicationController
   end
 
   def destroy
-    
-    @question= Question.find(params[:question_id])
+
     @question.destroy!
     redirect_to questions_path
   end
@@ -40,6 +44,29 @@ class QuestionsController < ApplicationController
 
   def question_params
     params.permit(:body, :test_id)
+  end
+
+  def find_question
+    @question = Question.find(params[:id])
+  end
+
+  def find_test
+    test = Test.find(params[:id])
+  end
+
+  def send_log_message
+    logger.info("Action [#{action_name}] was finished")
+  end
+
+  def log_execute_time
+    start = Time.now
+    yield
+    finish = Time.now - start
+    logger.info("Execute tome: #{finish *1000}ms")
+  end
+
+  def rescue_with_question_not_found
+    render plain: 'Question was not found'
   end
 
 end
