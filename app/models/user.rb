1406cce64a6d2ab75befeff0d79eb73d2d6a28f2
cyberriver <vblagirev@gmail.com>
2022-02-23 00:40:34 +0300

    Done 1st screencast + some addons concerning user name

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,16 +1,17 @@
+require 'uri'
+require 'digest/sha1'
+
 class User < ApplicationRecord
-  require 'uri'
+
+  include Auth
+
+  attr_reader :password
+  attr_writer :password_confirmation
+
   has_many :test_passages
   has_many :tests, through: :test_passages
   has_many :author_tests, class_name: :Test, foreign_key: :author_id
-  validates :name, presence: true
-  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
 
-  scope :show_tests, -> (level,user_id) do
-    joins(:users_tests, :tests)
-    .where(users_tests: {user_id: user_id}, tests: {level: level})
-  end
-  
   def test_passage(test)
     test_passages.order(id: :desc).find_by(test_id: test.id)
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
+  get 'users/new'
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
  root to: 'tests#index'
+
+ get :signup, to: 'users#new'
+
+ resources :users, only: :create
 
   resources :tests  do
     resources :questions, shallow: true, expect: :index do
