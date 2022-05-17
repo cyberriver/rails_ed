b1aa8ab3a84ffd9bc7efd66f0410c6e33a480989
cyberriver <vblagirev@gmail.com>
2022-05-17 18:39:04 +0300

    исправил замечания

--- app/javascript/scripts/check_pass.js
+++ app/javascript/scripts/check_pass.js
@@ -1,9 +1,7 @@
 
 document.addEventListener('turbo:load',function(){
   var control = document.querySelector('.pass-check')
-
   if (control) {control.addEventListener('input',passwordCheck)}
-
 })
 
 function passwordCheck(){
@@ -12,22 +10,19 @@ function passwordCheck(){
   var password = document.getElementById("user_password")
   var passwordConfirmation = document.getElementById("user_password_confirmation")
 
-  if (passwordConfirmation === "") {
-    passCheckSuccess.classList.add('hide')
-    passCheckFailed.classList.add('hide')
-  }
-
-
-
-      if (passwordConfirmation.value===password.value) {
-        console.log('пароли совпадают')
-        passCheckSuccess.classList.remove('hide')
-        passCheckFailed.classList.add('hide')
-      }else {
-        passCheckFailed.classList.remove('hide')
-        passCheckSuccess.classList.add('hide')
-        console.log('пароли не  совпадают')
-      }
-
-
+  switch (passwordConfirmation){
+    case "":
+      passCheckSuccess.classList.add('hide')
+      passCheckFailed.classList.add('hide')
+      break
+    case password.value:
+      console.log('пароли совпадают')
+      passCheckSuccess.classList.remove('hide')
+      passCheckFailed.classList.add('hide')
+      break
+    default:
+      passCheckFailed.classList.remove('hide')
+      passCheckSuccess.classList.add('hide')
+      console.log('пароли не  совпадают')
+      break
   }
