26b0723fbc14fbd3c5b8101fc918413beeb6a96b
cyberriver <vblagirev@gmail.com>
2022-08-15 20:12:39 +0300

    Revert "Отрефакторил рассылку почты"
    This reverts commit 213eee691cebb2df1fdde67ae3699406927116fb.

--- app/mailers/tests_mailer.rb
+++ app/mailers/tests_mailer.rb
@@ -3,7 +3,6 @@ class TestsMailer < ApplicationMailer
   def completed_test(test_passage)
     @user = test_passage.user
     @test = test_passage.test
-    @test_passage = test_passage
 
     mail to: @user.email
 
