143418ba898a412e90835ea16f4f1f7a1960ca4b
cyberriver <vblagirev@gmail.com>
2022-11-20 21:52:40 +0300

    fixed repo after deploy

--- db/migrate/20211231170202_create_tests.rb
+++ db/migrate/20211231170202_create_tests.rb
@@ -3,7 +3,7 @@ class CreateTests < ActiveRecord::Migration[7.0]
     create_table :tests do |t|
       t.string :title, limit: 1000, null: false
       t.integer :level, null: false, default: 0
-      t.references :category, null: false, foreign_key: true
+      
 
       t.timestamps
     end
---
+++ test/models/add_reference_tests_to_category_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class AddReferenceTestsToCategoryTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
